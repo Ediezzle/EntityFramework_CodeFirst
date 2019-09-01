f3bd8e7a160773a59972efaaeb84469ebc9feb0a
Ediezzle <edmore.mandikiyana.gmail.com>
2019-09-01 23:55:06 +0200

    first commitment

---
+++ EntityFramework_CodeFirst/CodeFirstContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EntityFramework_CodeFirst
+{
+    class CodeFirstContext : DbContext
+    {
+        public CodeFirstContext() : base("name = CodeFirstContextDB") { }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
+
